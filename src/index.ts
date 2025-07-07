fd525cbb62450954451bf3ba40fe34cc4751c0cc
Ashish Shrestha <ashish.shrestha@truenary.com>
2025-07-07 17:42:11 +0545

    feat(cors): implement cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,11 @@ config();
 const app = express();
 const PORT = parseInt(process.env.PORT ?? "5000", 10);
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 // @Initialize database table
